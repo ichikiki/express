df4da165922dda14db88cff4ad1c46b15dc90e41
ichikiki <wakana1626@gmail.com>
2025-11-25 19:57:32 +0900

    add dog api

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://ichikiki:vm2oWqtCj2EAiCZC@cluster0.f2mvlad.mongodb.net/?appName=Cluster0";
+const uri = "";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
