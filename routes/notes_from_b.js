25f96516a16c7876f8304683d67e240f17ef228d
ichikiki <wakana1626@gmail.com>
2026-01-19 19:46:15 +0900

    final update

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -5,7 +5,7 @@ const cors = require('cors'); // corsミドルウェアを追加
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 // corsミドルウェアを使用
